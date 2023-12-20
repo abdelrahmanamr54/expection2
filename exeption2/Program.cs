f253af617ba0ef7c946cd3a93345ad0269566603
abdelrahmanamr54 <amrmmi22@gmail.com>
2023-12-20 23:18:56 +0200

    first commit

---
+++ exeption2/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace exeption2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            try
+            {
+                string input =Convert.ToString(Console.ReadLine());
+                ContainsVowels(input);
+                Console.WriteLine("The input contains vowels.");
+            }
+            catch (novolwes ex)
+            {
+                Console.WriteLine(
+                    "error"
+
+
+
+                    
+                    
+                    
+                    );
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred:done");
+            }
+
+        }
+
+        static bool ContainsVowels(string input)
+        {
+            foreach (char c in input.ToLower())
+            {
+                if ("aeiou".Contains(c))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
